983a1a075f71fefb0b915bffaec4cae2118784f1
unknown <easyforlev@gmail.com>
2023-01-23 23:01:34 +0700

    delete some files

--- index.js
+++
@@ -1,9 +0,0 @@
-timersDiv.addEventListener('click', function(e){
-	e.stopPropagation();
-
-	const clicked = e.target.closest('.func-item');
-	let data = clicked.dataset.time;
-	if(!clicked) return;
-	console.log(Number(data));
-	countTimer = Number(data);
-});
